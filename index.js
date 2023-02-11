f47ecc65dc08c10c69732ae8b6ce9e80125804f7
Rahuldmc <115766755+Rahuldmc@users.noreply.github.com>
2023-02-11 21:01:40 +0530

    create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+let homescore = 0
+let guestscore = 0
+
+function add1home(){
+    homescore+=1
+    document.getElementById("homescore").innerText= homescore
+}
+
+function add2home(){
+    homescore+=2
+    document.getElementById("homescore").innerText= homescore
+}
+
+function add3home(){
+    homescore+=3
+    document.getElementById("homescore").innerText= homescore
+}
+
+function add1guest(){
+    guestscore+=1
+    document.getElementById("guestscore").innerText= guestscore
+}
+
+function add2guest(){
+    guestscore+=2
+    document.getElementById("guestscore").innerText= guestscore
+}
+
+function add3guest(){
+    guestscore+=3
+    document.getElementById("guestscore").innerText= guestscore
+}
